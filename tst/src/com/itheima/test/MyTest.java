d352783dc23d0d91ec375da67fcd4f5e466b930a
NewBlack51 <1154564163@qq.com>
2018-03-09 16:30:14 +0800

    init

---
+++ tst/src/com/itheima/test/MyTest.java
@@ -0,0 +1,12 @@
+package com.itheima.test;
+
+/**
+ * ClassName:MyTest <br/>
+ * Function: <br/>
+ * Date: 2018年3月9日 下午4:29:14 <br/>
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
